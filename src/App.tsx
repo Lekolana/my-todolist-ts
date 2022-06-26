492e1d20f006280ff16ba673f6f7b6892713c9bb
Svitlana <you@example.comlekontsevalana@gmail.com>
2022-06-26 14:06:37 +0200

    changes in App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+const App: React.FC= () => {
   return (
     <div className="App">
-    
+    <h1>"Hello"</h1>
     </div>
   );
 }
